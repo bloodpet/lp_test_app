a967548fad41ee463a1e4c7f632cf750d9dd6632
Eman Calso <egcalso@gmail.com>
2016-04-28 17:30:19 +0800

    Add polymer-based template for generating url

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from unsubscribe import urls as unsubscribe_urls
+from unsubscribe.views import UnsubscribeHomeView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'test_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', UnsubscribeHomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^unsubscribe/', include(unsubscribe_urls, namespace='unsubscribe')),
 )
